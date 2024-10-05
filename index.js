b221acbf2e1292a19b72e5f5ff9d728012249fc0
Mauro Coniglio <mauroconiglioo48@gmail.com>
2024-10-05 18:18:53 -0300

    descargue las extenciones que usariamos en el proyecto

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from 'express'
 
-console.log('hola mundo')
 // 1-configurar un puerto
 
 const app = express();
 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), ()=>{
-    console.info('Estoy escuchando el puerto' +app.get('port'))
+    console.info('Estoy escuchando el puerto '+ app.get('port'))
 })
+
 // 2- configurar middlewares
 
 // 3- configurar las rutas
-
+//http://localhost:4000/prueba
+ app.get('/prueba', (req,res)=>{
+    console.log('alguien hizo una solicitud get a la ruta de prueba')
+    res.send('Hola miundo desde el backend')
+ })
