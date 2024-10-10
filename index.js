e9f43ab14dc384c178eb02a56a040c88e79c69c4
Mauro Escritorio <mauroconiglioo48@gmail.com>
2024-10-09 21:02:51 -0300

    agregamos middlewares, carpeta public y el index

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import express from 'express'
+import express from 'express';
+import morgan from 'morgan';
+import cors from  'cors';
 
 console.log('hola mundo')
 // 1-configurar un puerto
@@ -7,9 +9,18 @@ const app = express();
 
 app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), ()=>{
-    console.info('Estoy escuchando el puerto' +app.get('port'))
+    console.info('Estoy escuchando el puerto ' +app.get('port'))
 })
 // 2- configurar middlewares
+app.use(cors())//permite conecciones remotas
+app.use(morgan('dev'))//da informacion extra en la terminal
+app.use(express.json())//interpreta los datos en formato json de la solicitud 
+app.use(express.urlencoded({extended:true}))
 
 // 3- configurar las rutas
+//http://localhost:4000/prueba
+app.get('/prueba',(req,res) => {
+    console.log('alguien hizo una solicitud get a la ruta de prueba')
+    res.send('Hola mundo desde el backend')
+})
 
