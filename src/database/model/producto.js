b7dd58c940cfff28869ed63672afd7b21762ec72
Mauro Escritorio <mauroconiglioo48@gmail.com>
2024-10-13 12:10:09 -0300

    agregamos el modelo de producto

--- src/database/model/producto.js
+++ src/database/model/producto.js
@@ -13,5 +13,40 @@ const productoSchema = new Schema ({
         required:true,
         minLength:50,
         maxLength:1000000
+    },
+    imagen:{
+        type:String,
+        required:true,
+        validate:{
+            validator: (valor)=>{
+                /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png)/.test(valor)    
+            }
+        }
+    },
+    categoria:{
+        type:String,
+        required:true,
+        enum:['Arte','Cuadernos','Escritura','Hojas'],
+    },
+    marca:{
+        type:String,
+        required:true,
+        minLength:2,
+        maxLength:50,
+    },
+    descripcion_breve:{
+        type:String,
+        required:true,
+        minLength:3,
+        maxLength:50,
+    },
+    descripcion_amplia:{
+        type:String,
+        required:true,
+        minLength:50,
+        maxLength:1000,
     }
 })
+
+const Producto = mongoose.model('producto',productoSchema);
+export default Producto;
