033b942a429597f5cd32d9249b2dd91fbe56839e
Mauro Escritorio <mauroconiglioo48@gmail.com>
2024-10-19 13:50:56 -0300

    verificamos si el producto existe y luego actuializamos el producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -37,3 +37,28 @@ export const listarProductos = async (req, res) => {
         .json({ mensaje: "Ocurrio un error, no se puede crear el producto" });
     }
   };
+
+  export const editarProducto = async(req,res)=>{
+      try {
+
+      //validar los datos del body
+      //buscar si el producto existe
+      const productoBuscado = await Producto.findById(req.params.id)
+  
+      //en caso de que no exista contsto con un error
+        if(!productoBuscado){
+           return res.status(404).json({mensaje:'El producto solicitado no existe'})
+        }
+          //si lo encontre al producto, entonces lo edito
+          await Producto.findByIdAndUpdate(req.params.id, req.body)
+          res.status(200).json({mensaje:'El producto fue editado correctamente'})
+        
+      
+      //envio respuesta al front end
+      } catch (error) {
+        console.error(error)
+        res
+        .status(500)
+        .json({ mensaje: "Ocurrio un error, no se pudo editar el producto" });
+      }
+  }
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   crearProducto,
+  editarProducto,
   funcionPrueba,
   listarProductos,
 } from "../controllers/productos.controllers.js";
@@ -8,4 +9,5 @@ import {
 const router = Router();
 router.route("/prueba").get(funcionPrueba);
 router.route("/productos").post(crearProducto).get(listarProductos);
+router.route("/productos/:id").put(editarProducto)
 export default router;
